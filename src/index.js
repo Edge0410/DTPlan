17b7e9c70c2e4fdcecbf769433618e36333a99af
EDY\edial <edialexandru.es@gmail.com>
2023-04-10 11:16:29 +0300

    Added Login & Register system

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ const fs = require('fs');
 const crypto = require('crypto');
 const session = require('express-session');
 
+var database = require('./database');
+
 app = express();
 app.set("view engine", "ejs"); 
 
 app.use("/assets", express.static(__dirname+"/assets"));
+
 app.use(express.urlencoded({extended:true})); // creeaza req.body pentru formular ca sa nu mai fie in url parametrii din post
 
 app.use(session({ secret: 'abcdefg', resave: true, saveUninitialized: false})); // req.session
@@ -16,16 +19,71 @@ app.get(["/", "/index"], function(req, res) {
     if(req.session.found == -1) {
         req.session.found = 0;
         req.session.save();
-        
     }
 });
 
-app.get(["/login"], function(req, res) {
+app.get(["/login"], function(req, res){
     res.render("pages/login");
 });
 
-app.get(["/register"], function(req, res) {
+app.get(["/register"], function(req, res){
     res.render("pages/register");
+});
+
+app.post(["/login"], function(req, res){
+    console.log(req.body);
+    var user = req.body.username;
+    var password = crypto.scryptSync(req.body.password, "iewhrg3yYYDAjert377999", 32).toString('hex');
+    console.log(password);
+
+    query = `
+    SELECT * FROM users
+    where username = "${user}"
+    `;
+
+    database.query(query, function(err, resq){
+        if(resq.length > 0)
+        {
+            if(resq[0].password == password)
+            {
+                req.session.user = user;
+                req.session.found = 1;
+                res.redirect("/index");
+            }
+            else
+            {
+                req.session.found = -1;
+                res.redirect("/index");
+            }
+        }
+        else
+        {
+                req.session.found = -1;
+                res.redirect("/index");
+        }
+    });
+
+    console.log(req.session.user);
+
+});
+
+app.post("/register", function(req, res){
+    console.log(req.body);
+    var user = req.body.username;
+    var email = req.body.email;
+    var encpass = crypto.scryptSync(req.body.password, "iewhrg3yYYDAjert377999", 32).toString('hex');
+
+    query = `
+    INSERT INTO users (username, email, password)
+    values ("${user}", "${email}", "${encpass}")
+    `;
+
+    database.query(query, function(err, resq){
+        if (err) throw err;
+        console.log(resq.affectedRows);
+    });
+    res.redirect("/index");
+
 });
 
 app.listen(8888); 
