2dfb5f993b7791c298b21614481ff67ac3cabc94
EDY\edial <edialexandru.es@gmail.com>
2023-05-04 15:48:33 +0300

    Finished Create Plan Feature

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -33,3 +33,60 @@ function fetchPlans(route) {
       }, 100);
   });
 }
+
+
+function fetchPlanContent(route) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            $("#counter").html("");
+            $.ajax({
+                url: route,
+                type: "GET",
+                dataType: "json",
+                success: function (res) {
+                    let container = document.getElementsByClassName("container")[0];
+                    console.log(res.records.length);//1
+                    if(res.type == 0){ // diet plan
+                        for(let i=0; i<res.records.length; i++){
+                            let planChild = document.createElement('div');
+                            planChild.classList.add("diet-data");
+                            let childTitle = document.createElement('h3');
+                            childTitle.classList.add("diet-title");
+                            let childDesc = document.createElement('span');
+                            childDesc.classList.add("diet-desc");
+                            childTitle.innerHTML = res.records[i].title;
+                            childDesc.innerHTML = res.records[i].calories + " calorii";
+                            planChild.appendChild(childTitle);
+                            planChild.appendChild(childDesc);
+                            container.appendChild(planChild);
+                        }
+                    }
+                    else
+                    {
+                        for(let i=0; i<res.records.length; i++){
+                            let planChild = document.createElement('div');
+                            planChild.classList.add("workout-data");
+                            let childTitle = document.createElement('h3');
+                            childTitle.classList.add("diet-title");
+                            let childDesc = document.createElement('span');
+                            childDesc.classList.add("diet-desc");
+                            childTitle.innerHTML = res.records[i].title;
+                            childDesc.innerHTML = "Nivel de dificultate: " + res.records[i].difficulty;
+                            planChild.appendChild(childTitle);
+                            planChild.appendChild(childDesc);
+                            container.appendChild(planChild);
+                        }
+                    }  
+                    
+                },
+            });
+            resolve('updated plan list');
+        }, 100);
+    });
+  }
+
+  function removeValidation() {
+    // remove the "required" attribute from the input and textarea elements
+        document.getElementById("name").removeAttribute("required");
+        document.getElementById("description").removeAttribute("required");
+        }
--- src/index.js
+++ src/index.js
@@ -7,26 +7,26 @@ const session = require('express-session');
 var database = require('./database');
 
 app = express();
-app.set("view engine", "ejs"); 
+app.set("view engine", "ejs");
 
-app.use("/assets", express.static(__dirname+"/assets"));
+app.use("/assets", express.static(__dirname + "/assets"));
 
 app.use(cors());
 
-app.use(express.urlencoded({extended:true})); // creeaza req.body pentru formular ca sa nu mai fie in url parametrii din post
+app.use(express.urlencoded({ extended: true })); // creeaza req.body pentru formular ca sa nu mai fie in url parametrii din post
 
-app.use(session({ secret: 'abcdefg', resave: true, saveUninitialized: false})); // req.session
+app.use(session({ secret: 'abcdefg', resave: true, saveUninitialized: false })); // req.session
 
-app.get(["/", "/index"], function(req, res) {
-        console.log(req.session.userid);
-        res.render("pages/index", {id:req.session.userid, user:req.session.user, found:req.session.found});
-        if(req.session.found == -1) {
-            req.session.found = 0;
-            req.session.save();
-        }
+app.get(["/", "/index"], function (req, res) {
+    console.log(req.session.userid);
+    res.render("pages/index", { id: req.session.userid, user: req.session.user, found: req.session.found });
+    if (req.session.found == -1) {
+        req.session.found = 0;
+        req.session.save();
+    }
 });
 
-app.get("/logout", function(req,res){
+app.get("/logout", function (req, res) {
     req.session.destroy();
     res.redirect("/index");
 });
@@ -36,19 +36,18 @@ app.get("/fetch-wplans", function (req, res) {
     SELECT * FROM workout_plans where user_id = "${req.session.userid}"
     `
 
-    database.query(query, function(err, resq){
-        if (err){
+    database.query(query, function (err, resq) {
+        if (err) {
             res.json({
-                msg: error,
+                msg: err,
                 records: 0,
-              })
+            })
         }
-        else
-        {
+        else {
             res.json({
                 msg: 'Data successfully fetched',
                 records: resq,
-              })
+            })
         }
     });
 });
@@ -58,46 +57,80 @@ app.get("/fetch-dplans", function (req, res) {
     SELECT * FROM diet_plans where user_id = "${req.session.userid}"
     `
 
-    database.query(query, function(err, resq){
-        if (err){
+    database.query(query, function (err, resq) {
+        if (err) {
             res.json({
-                msg: error,
+                msg: err,
                 records: 0,
-              })
+            })
         }
-        else
-        {
+        else {
             res.json({
                 msg: 'Data successfully fetched',
                 records: resq,
-              })
+            })
         }
     });
 });
 
-app.get("/create-plan", function(req, res){
-    res.render("pages"+req.url, {id:req.session.userid, user:req.session.user, found:req.session.found}, function(err, rezrand){
-        if(err){
-            res.render("pages/error404", {id:req.session.userid, user:req.session.user, found:req.session.found}); 
+app.get("/fetch-cache-plan", function (req, res) {
+    let planType = req.session.planType;
+    let query;
+    console.log(req.session.insertId);
+    console.log(req.session.planType);
+
+    if (planType == 0) {
+        query = `
+        SELECT * FROM diet_plans dp join diet_meals dm on dp.id = dm.id_diet join meals m on dm.id_meals = m.id where dp.id = "${req.session.insertId}"`;
+    }
+    else {
+        query = `
+        SELECT * FROM workout_plans dp join workout_exercises dm on dp.id = dm.id_workout join exercises m on dm.id_exercises = m.id where dp.id = "${req.session.insertId}"`;
+    }
+
+    //req.session.insertId = null; 
+    // se scoate daca vrem sa ne ramana ultimul plan in cache toata sesiunea
+    database.query(query, function (err, resq) {
+        if (err) {
+            res.json({
+                msg: err,
+                type: undefined,
+                records: 0,
+            })
         }
-        else if(!req.session.user)
-        {
-            res.redirect("/login"); 
+        else {
+            res.json({
+                msg: '',
+                type: planType,
+                records: resq,
+            })
         }
-        else
-        {
+    });
+});
+
+app.get(["/create-plan", "/complete-create-plan"], function (req, res) {
+    res.set('Cache-Control', 'no-store, no-cache');
+    res.render("pages" + req.url, { id: req.session.userid, user: req.session.user, found: req.session.found }, function (err, rezrand) {
+        if (err) {
+            res.render("pages/error404", { id: req.session.userid, user: req.session.user, found: req.session.found });
+        }
+        else if (!req.session.user) {
+            res.redirect("/login");
+        }
+        else {
             res.send(rezrand);
         }
     });
 });
 
-app.get("/*", function(req, res){
-    res.render("pages"+req.url, {id:req.session.userid, user:req.session.user, found:req.session.found}, function(err, rezrand){
-        if(err){
-            res.render("pages/error404", {id:req.session.userid, user:req.session.user, found:req.session.found}); 
+
+app.get("/*", function (req, res) {
+    res.set('Cache-Control', 'no-store, no-cache');
+    res.render("pages" + req.url, { id: req.session.userid, user: req.session.user, found: req.session.found, plancacheid: req.session.insertId, plancachetype: req.session.planType}, function (err, rezrand) {
+        if (err) {
+            res.render("pages/error404", { id: req.session.userid, user: req.session.user, found: req.session.found });
         }
-        else
-        {
+        else {
             res.send(rezrand);
         }
     });
@@ -113,7 +146,7 @@ app.get(["/register"], function(req, res){
 });
 */
 
-app.post("/login", function(req, res){
+app.post("/login", function (req, res) {
     console.log(req.body);
     var user = req.body.username;
     var password = crypto.scryptSync(req.body.password, "iewhrg3yYYDAjert377999", 32).toString('hex');
@@ -124,34 +157,30 @@ app.post("/login", function(req, res){
     where username = "${user}"
     `;
 
-    database.query(query, function(err, resq){
-        if(resq.length > 0)
-        {
-            if(resq[0].password == password)
-            {
+    database.query(query, function (err, resq) {
+        if (resq.length > 0) {
+            if (resq[0].password == password) {
                 req.session.user = user;
                 console.log(resq[0].id);
                 req.session.userid = resq[0].id;
                 req.session.found = 1;
                 res.redirect("/index");
             }
-            else
-            {
+            else {
                 req.session.found = -1;
                 res.redirect("/index");
             }
         }
-        else
-        {
-                req.session.found = -1;
-                res.redirect("/index");
+        else {
+            req.session.found = -1;
+            res.redirect("/index");
         }
     });
 
     console.log(req.session.user);
 });
 
-app.post("/complete-register",function(req,res){
+app.post("/complete-register", function (req, res) {
     console.log(req.body);
     var weight = req.body.weight;
     var height = req.body.height;
@@ -159,7 +188,7 @@ app.post("/complete-register",function(req,res){
     var birth_date = new String(req.body.birthday);
     console.log(birth_date);
     var intgender;
-    if(gender == "male")
+    if (gender == "male")
         intgender = 1;
     else
         intgender = 0;
@@ -169,7 +198,7 @@ app.post("/complete-register",function(req,res){
     set weight = ${weight}, height = ${height}, gender = ${intgender}, birth_date = STR_TO_DATE('${birth_date}', '%Y-%m-%d')
     where id = ${req.session.userid};`
 
-    database.query(query, function(err, resq){
+    database.query(query, function (err, resq) {
         if (err) throw err;
         console.log(resq.affectedRows);
     });
@@ -177,18 +206,18 @@ app.post("/complete-register",function(req,res){
     res.redirect("/index");
 })
 
-app.post("/register", function(req, res){
+app.post("/register", function (req, res) {
     console.log(req.body);
     var user = req.body.username;
     var email = req.body.email;
     var encpass = crypto.scryptSync(req.body.password, "iewhrg3yYYDAjert377999", 32).toString('hex');
-    
+
     query = `
     INSERT INTO users (username, email, password)
     values ("${user}", "${email}", "${encpass}")
     `;
 
-    database.query(query, function(err, resq){
+    database.query(query, function (err, resq) {
         if (err) throw err;
         console.log(resq.affectedRows);
     });
@@ -198,14 +227,153 @@ app.post("/register", function(req, res){
     where username = "${user}"
     `;
 
-    database.query(query2, function(err, resq){
-                req.session.user = user;
-                console.log(resq[0].id);
-                req.session.userid = resq[0].id;
-                req.session.found = 1; 
-                res.redirect("/complete-register");          
+    database.query(query2, function (err, resq) {
+        req.session.user = user;
+        console.log(resq[0].id);
+        req.session.userid = resq[0].id;
+        req.session.found = 1;
+        res.redirect("/complete-register");
     })
 });
 
-app.listen(8888); 
+app.post("/create-plan", function (req, res) {
+    console.log(req.body);
+    var ptype = req.body.ptype;
+    var height = req.body.height;
+    var weight = req.body.weight;
+    var age = req.body.age;
+    var routine = req.body.routine;
+    var goal = req.body.goal;
+    var inserted_id;
+
+    if (ptype == 0) { // avem diet plan
+        queryCreatePlan = `INSERT INTO diet_plans (name, description, user_id) values ("", "", ${req.session.userid})`;
+        database.query(queryCreatePlan, function (err, resq) {
+            if (err) {
+                res.redirect("error404");
+            } else {
+                inserted_id = resq.insertId;
+                req.session.insertId = inserted_id;
+                req.session.planType = ptype;
+                console.log(inserted_id);
+                /* algoritm */
+
+                query = `INSERT INTO diet_meals VALUES (${inserted_id}, 1), (${inserted_id}, 2), (${inserted_id}, 3)`;
+
+                /* algoritm */
+
+                database.query(query, function (err, resq) {
+                    if (err) 
+                        res.redirect("error404");
+                    else {
+                        res.redirect("/complete-create-plan");
+                    }
+                });
+            }
+        })
+    }
+    else
+    {
+        queryCreatePlan = `INSERT INTO workout_plans (name, description, user_id) values ("", "", ${req.session.userid})`;
+        database.query(queryCreatePlan, function (err, resq) {
+            if (err) {
+                throw err;
+            } else {
+                inserted_id = resq.insertId;
+                req.session.insertId = inserted_id;
+                req.session.planType = ptype;
+                console.log(inserted_id);
+                /* algoritm */
+
+                query = `INSERT INTO workout_exercises VALUES (${inserted_id}, 1), (${inserted_id}, 2), (${inserted_id}, 3)`;
+
+                /* algoritm */
+
+                database.query(query, function (err, resq) {
+                    if (err) throw err;
+                    else {
+                        res.redirect("/complete-create-plan");
+                    }
+                });
+            }
+        })
+    }
+});
+
+app.post("/update-created-plan", function(req, res){
+    console.log(req.body);
+    var name = req.body.name;
+    var desc = req.body.description;
+    var action = req.body.submit;
+    if(action == 0){ //delete plan
+        if(req.session.planType == 0)
+        {
+            query = `DELETE from diet_meals where id_diet = ${req.session.insertId}`;
+            database.query(query, function(err, resq){
+                if (err) {
+                    throw err;
+                } else {    
+                    query = `DELETE from diet_plans where id = ${req.session.insertId}`;
+                    database.query(query, function (err, resq) {
+                        if (err) throw err;
+                        else {
+                            req.session.insertId = null;
+                            req.session.planType = null;
+                            res.redirect("/diet-plans");
+                        }
+                    });
+                }
+            })
+        }
+        else
+        {
+            query = `DELETE from workout_exercises where id_workout = ${req.session.insertId}`;
+            database.query(query, function(err, resq){
+                if (err) {
+                    throw err;
+                } else {  
+                    query = `DELETE from workout_plans where id = ${req.session.insertId}`;
+                    database.query(query, function (err, resq) {
+                        if (err) throw err;
+                        else {
+                            req.session.insertId = null;
+                            req.session.planType = null;  
+                            res.redirect("/workout-plans");
+                        }
+                    });
+                }
+            })
+        }
+    }
+    else{ // update name & desc
+        if(req.session.planType == 0)
+        {
+            query = `UPDATE diet_plans set name = "${name}", description = "${desc}" where id = ${req.session.insertId}`;
+            database.query(query, function (err, resq) {
+                if (err) throw err;
+                else {
+                    req.session.insertId = null;
+                    req.session.planType = null;
+                    res.redirect("/diet-plans");
+                }
+            });
+        }
+        else
+        {
+            query = `UPDATE workout_plans set name = "${name}", description = "${desc}" where id = ${req.session.insertId}`;
+            database.query(query, function (err, resq) {
+                if (err) throw err;
+                else {
+                    req.session.insertId = null;
+                    req.session.planType = null;
+                    res.redirect("/workout-plans");
+                }
+            });
+        }
+    }
+});
+
+
+
+app.listen(8888);
 console.log("Running...");
